743fcf7e358669b60d8c2695033517873050c884
hasithakms <hasithakms@gmail.com>
2020-06-10 16:11:02 +0530

    Add project files.

---
+++ Movies-RazorPages/Pages/Movies/Delete.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Movies_RazorPages.Data;
+using Movies_RazorPages.Models;
+
+namespace Movies_RazorPages
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly Movies_RazorPages.Data.Movies_RazorPagesContext _context;
+
+        public DeleteModel(Movies_RazorPages.Data.Movies_RazorPagesContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Movie = await _context.Movie.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Movie == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Movie = await _context.Movie.FindAsync(id);
+
+            if (Movie != null)
+            {
+                _context.Movie.Remove(Movie);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ Movies-RazorPages/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Movies_RazorPages.Data;
+using Movies_RazorPages.Models;
+
+namespace Movies_RazorPages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Movies_RazorPages.Data.Movies_RazorPagesContext _context;
+
+        public IndexModel(Movies_RazorPages.Data.Movies_RazorPagesContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movie.ToListAsync();
+        }
+    }
+}
